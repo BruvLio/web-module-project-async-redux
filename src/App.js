1cb1a7f3bf945f21294f12818d8b90ed307a625e
Lionel Mbayu <Lionel.Mbayu@gmail.com>
2023-01-13 13:47:40 -0600

    Made Api call, Connected Store, Displaying Characters names

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getAllCharacters } from './actions';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
+function App({ getAllCharacters, characters }) {
+
+  useEffect(() => {
+    getAllCharacters()
+  }, [])
+
   return (
     <div className="App">
       Async Redux Project
+
+    <div>Lionel
+      
+    </div>
+      <div className="Characters">
+        {characters.map(character => <h1>{character.name}</h1>)}
+      </div>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return ({
+    characters: state.characters
+  });
+}
+
+export default connect(mapStateToProps, { getAllCharacters })(App);
